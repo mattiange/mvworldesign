ed8f93fdc29498d1559c0c22ba3aaf13a7434559
mattiange <mattia.angelillo@gmail.com>
2018-10-04 13:30:06 +0200

    Added favicon Added logo

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -25,6 +25,26 @@ foreach ($mainmenu as $k_mn => $v_mn){
 <html lang="<?= Yii::$app->language ?>">
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
+    
+    <link rel="apple-touch-icon" sizes="57x57" href="<?= Yii::getAlias('@web') ?>/images/ico/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?= Yii::getAlias('@web') ?>/images/ico/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?= Yii::getAlias('@web') ?>/images/ico/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?= Yii::getAlias('@web') ?>/images/ico/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?= Yii::getAlias('@web') ?>/images/ico/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?= Yii::getAlias('@web') ?>/images/ico/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?= Yii::getAlias('@web') ?>/images/ico/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?= Yii::getAlias('@web') ?>/images/ico/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= Yii::getAlias('@web') ?>/images/ico/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="<?= Yii::getAlias('@web') ?>/images/ico/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= Yii::getAlias('@web') ?>/images/ico/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?= Yii::getAlias('@web') ?>/images/ico/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= Yii::getAlias('@web') ?>/images/ico/favicon/favicon-16x16.png">
+    
+    <link rel="manifest" href="<?= Yii::getAlias('@web') ?>/images/ico/favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="<?= Yii::getAlias('@web') ?>/images/ico/favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+    
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
@@ -49,15 +69,15 @@ foreach ($mainmenu as $k_mn => $v_mn){
                 </button>
 
                 <!-- Logo Image -->
-                <a class="navbar-brand" href="<?= Url::home(); ?>">
-                    <img src="<?= Yii::getAlias('@web') ?>/images/ico/credits/vera-bracco-graphic-designer.png" alt="logo" role="banner" alt="Vera Bracco Graphic Designer" />
-                </a>
+                <!--<a class="navbar-brand" href="<?= Url::home(); ?>">
+                    <img src="<?= Yii::getAlias('@web') ?>/" alt="logo" role="banner" alt="Vera Bracco Graphic Designer" />
+                </a>-->
 
             </div>
             <!-- Navigation Menu -->
             <?php
             NavBar::begin([
-                'brandLabel' => 'M&V World Design',
+                'brandLabel' => Html::img('@web/images/ico/logo/png/logo-MV-bianco-su-sfondo-nero__100x71.png', ['alt'=>'Logo M&V World Design', 'title' => 'M&V World Design']),
                 'brandUrl' => Yii::$app->homeUrl,
                 'options' => [
                     'class' => 'navbar-inverse navbar-fixed-top',
