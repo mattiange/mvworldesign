c4b8181c1a6d2162086107619b12f916a6c4964c
mattiange <mattia.angelillo@gmail.com>
2018-10-12 13:07:27 +0200

    Email template

--- common/mail/layouts/html.php
+++ common/mail/layouts/html.php
@@ -12,10 +12,27 @@ use yii\helpers\Html;
     <meta http-equiv="Content-Type" content="text/html; charset=<?= Yii::$app->charset ?>" />
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
+    <style type="text/css">
+        #html-email{
+        }
+        #html-email .logo{
+            background-image: url(../../../frontend/web/images/ico/logo/jpg/logo-MV-oro-su-sfondo-bianco-.jpg);
+            background-size: 100%;
+            background-repeat: no-repeat;
+            width: 500px;
+            height: 500px;
+            margin: 0 auto;
+        }
+    </style>
 </head>
 <body>
     <?php $this->beginBody() ?>
-    <?= $content ?>
+    <div id="html-email">
+        <div class="logo"></div>
+        <div class="content">
+            <?= $content ?>
+        </div>
+    </div>
     <?php $this->endBody() ?>
 </body>
 </html>
