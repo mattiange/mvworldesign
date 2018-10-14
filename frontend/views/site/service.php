aab26445603e3c580ba17e76664010ce548e642a
mattiange <mattia.angelillo@gmail.com>
2018-10-14 16:17:14 +0200

    Added Service

--- frontend/views/site/service.php
+++ frontend/views/site/service.php
@@ -1,4 +1,13 @@
 <?php
 $this->title = $model[0]['service'].Yii::$app->name;
+$this->params['breadcrumbs'][] = $model[0]['service'];
+$this->params['breadcrumbs'][] = 'Web Design';
+$this->registerMetaTag([
+    'name' => 'description',
+    'content' => $model[0]['meta_description'],
+]);
 ?>
-<?= $model[0]['text'] ?>
+<h1 class="text-center"><?= $model[0]['service'] ?></h1>
+<div id="services" class="wrapper margin-0-a">
+    <?= $model[0]['text'] ?>
+</div>
