22b18aed41534a32f5e0708fffe71b09eab870bb
Davide <Davide@DESKTOP-AJ011RI>
2018-10-31 00:54:27 +0100

    Added keywords on graphic's page

--- frontend/views/site/graphic.php
+++ frontend/views/site/graphic.php
@@ -5,6 +5,10 @@ $this->registerMetaTag([
     'name' => 'description',
     'content' => 'Realizziamo il tuo logo aziendale con presentazione e immagine coordinata.  Vuoi aprire un giornale? Noi facciamo al caso tuo,scrivici info@mvworldesign.com',
 ]);
+$this->registerMetaTag([
+    'name' => 'keywords',
+    'content' => 'grafica editoriale, grafica pubblicitaria, immagine coordinata, fotoritocco, come funziona photoshop, come fotoritoccare una foto',
+]);
 ?>
 <h1 class="text-center"><?= Yii::t('app', 'Graphic Design') ?></h1>
 
