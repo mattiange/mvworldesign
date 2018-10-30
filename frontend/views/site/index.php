8179dee6616bf786af15e851544517e25a940cbf
mattiange <mattia.angelillo@gmail.com>
2018-10-30 15:43:13 +0100

    Add services on homepage

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,4 +1,7 @@
 <?php
+use frontend\models\Services;
+use yii\helpers\Url;
+
 /*use yii2tech\sitemap\File;
 $siteMapFile = new File();
 $siteMapFile->writeUrl(['site/index'], ['priority' => '0.9']);
@@ -44,9 +47,42 @@ $this->registerMetaTag([
             </div>
         </div>
     </div>
+    
+    <div class="col-sm-12">
+        <section class="services">
+            <div class="auto-box">
+                <h1 class="border-line-center text-center">
+                    <?= Yii::t('app', 'I nostri servizi') ?>
+                </h1>
+
+                <div class="row clearfix">
+                    <?php foreach (Services::find()->all() as $k_cat => $cat) : ?>
+                    <div class="box col-lg-4 col-md-4 col-sm-6 col-xs-12 anim-3-all animated pulse animated-delay-2" 
+                         data-delay="0" 
+                         data-animation="pulse">
+                        <span class="icon">
+                            <img src="<?= Yii::getAlias('@web').'/uploads/'.$cat->cover ?>" alt="" />
+                        </span>
+
+                        <h3>
+                            <a href="<?= Url::toRoute(['site/category', 'id' => $cat->id]); ?>">
+                                <?= $cat->service ?>
+                            </a>
+                        </h3>
+
+                        <div class="content"><?= $cat->intro_text ?></div>
+                    </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        </section>
+    </div>
 </section>
 <?php
 $this->registerCssFile(Yii::getAlias('@web').'/css/home-widget.css', [
+    'depends' => 'yii\bootstrap\BootstrapAsset',
+]);
+$this->registerCssFile(Yii::getAlias('@web').'/css/services.css', [
     'depends' => 'yii\bootstrap\BootstrapAsset',
 ]);
 $this->registerJsFile(Yii::getAlias('@web').'/js/home-widget.jquery.js?v=1',  [
