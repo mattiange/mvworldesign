48351aa18a71a5843c3045968e0315ca26a80fcc
mattiange <mattia.angelillo@gmail.com>
2018-10-31 13:03:14 +0100

    Modified portfolio
    Added portfolio.js (jQuery's plugin)
    Splitted portfolio CSS rules into portfolio.css

--- frontend/web/js/portfolio.js
+++ frontend/web/js/portfolio.js
@@ -3,50 +3,31 @@
  * Revision: 0
  * Author: Mattia Leonardo Angelillo
  */
-jQuery(document).ready(function ($){
-    /**
-     * Show hoverlay text
-     */
-    $('#portfolio .item-overlay').bind({
-        mouseenter : function (e){
-            var this_width = $(this).width(),
-                this_height = $(this).height(),
-                overlayitem_width = $(".overlay-content", this).width(),
-                overlayitem_height = $(".overlay-content", this).height();
-            
-            $(this).css('opacity', 1);
-            $(".overlay-content", this).css({
-                bottom :(this_height-overlayitem_height)/2,
-                opacity : 1
-            });
-        },
-        mouseleave : function (e){
-            $(this).css('opacity', 0);
-        }
-    });
-    
-    /**
-     * Show selected category
-     */
-    var old_filter;
-    $("#portfolio [data-filter]").click(function (){
-        var filter = $(this).attr('data-filter');
+(function( $ ) {
+    $.fn.portfolio = function(options) {
+        var settings = $.extend({
+            overlayItem : '.item-overlay',
+            overlayContent : '.overlay-content'
+        }, options );
         
-        if (typeof old_filter === 'undefined' && filter !=="all") {
-            $(this).addClass('active');
-            $("#portfolio > [data-category!="+filter+"].filter").hide('drop', 500);
-        }
+        var _this   = $(this);
+        var _overlayItem = $(settings.overlayItem, _this);
+        var _overlayContent = $(settings.overlayContent, _this);
         
-        if(filter !== old_filter){
-            $("#portfolio [data-filter]").removeClass('active');
-            if(typeof old_filter !== 'undefined'){$(this).addClass('active');}
-            
-            old_filter = filter;
-            
-            $("#portfolio > .filter").show();
-            if(filter!=="all"){
-                $("#portfolio > [data-category!="+filter+"].filter").hide('drop', 500);
+        _overlayItem.bind({
+            mouseenter : function (){
+                var thisHeight = $(this).height(),
+                overlayItemHeight = _overlayContent.height();
+                
+                $(this).css('opacity', 1);
+                _overlayContent.css({
+                    bottom :(thisHeight-overlayItemHeight)/2,
+                    opacity : 1
+                });
+            },
+            mouseleave : function (){
+                $(this).css('opacity', 0);
             }
-        }
-    });
-});
+        });
+    };
+}(jQuery));
