ca712fb7ea8bfa7fd5bc74ec6d1ce61e57696cb7
mattiange <mattia.angelillo@gmail.com>
2018-10-31 13:49:54 +0100

    Added show categories

--- frontend/web/js/portfolio.js
+++ frontend/web/js/portfolio.js
@@ -7,14 +7,26 @@
     $.fn.portfolio = function(options) {
         var settings = $.extend({
             overlayItem : '.item-overlay',
-            overlayContent : '.overlay-content'
+            overlayContent : '.overlay-content',
+            filter : 'data-filter',
+            category : 'data-category'
         }, options );
         
-        var _this   = $(this);
-        var _overlayItem = $(settings.overlayItem, _this);
-        var _overlayContent = $(settings.overlayContent, _this);
+        var _this           = $(this);//Containers
+        var _overlayItem    = $(settings.overlayItem, _this);//Overlay element
+        var _overlayContent = $(settings.overlayContent, _this);//Overlay content
+        var _filter         = $("["+settings.filter+"]", _this);
+        var oldFilter       = null;
         
+        /**
+         * Portfolio's events
+         */
         _overlayItem.bind({
+            /**
+             * Mouse Enter
+             * 
+             * @returns {undefined}
+             */
             mouseenter : function (){
                 var thisHeight = $(this).height(),
                 overlayItemHeight = _overlayContent.height();
@@ -25,9 +37,44 @@
                     opacity : 1
                 });
             },
+            /**
+             * Mouse out
+             * 
+             * @returns {undefined}
+             */
             mouseleave : function (){
                 $(this).css('opacity', 0);
             }
         });
+        
+        filter();
+        
+        /**
+         * Category to show
+         * 
+         * @returns {undefined}
+         */
+        function filter(){
+            _filter.click(function (){
+                var filter = $(this).attr(settings.filter);
+                
+                if (typeof oldFilter === 'undefined' && filter !=="all") {
+                    $(this).addClass('active');
+                    $(" > ["+settings.category+"!="+filter+"].filter", _this).hide('drop', 500);
+                }
+
+                if(filter !== oldFilter){
+                    $(" ["+settings.filter+"]", _this).removeClass('active');
+                    if(typeof oldFilter !== 'undefined'){$(this).addClass('active');}
+
+                    oldFilter = filter;
+                    
+                    $("> .filter", _this).show();
+                    if(filter!=="all"){
+                        $("> ["+settings.category+"!="+filter+"].filter", _this).hide('drop', 500);
+                    }
+                }
+            });
+        }
     };
 }(jQuery));
