239019b3438bcb105ff7b6afdac8e7433fa2b63d
Davide <Davide@DESKTOP-AJ011RI>
2018-11-02 10:58:31 +0100

    Added fx on web's services

--- frontend/views/site/web.php
+++ frontend/views/site/web.php
@@ -18,7 +18,7 @@ $this->registerMetaTag([
     <div class="container margin-0-a">
         <div class="row margin-0-a">
             <?php foreach ($model as $k => $v): ?>
-            <div class="col-sm-6 col-md-6 col-lg-6">
+            <div class="col-sm-6 col-md-6 col-lg-6 animated">
                 <div class="cover col-sm-2">
                     <img src="<?= $v['cover'] ?>" src="" alt="" />
                 </div>
@@ -31,3 +31,30 @@ $this->registerMetaTag([
         </div>
     </div>
 </div>
+<?php
+$this->registerJsFile(Yii::getAlias('@web').'/js/fx.jquery.js',  [
+    'depends' => [
+        yii\web\JqueryAsset::className(),
+        yii\jui\JuiAsset::className()
+    ]
+]);
+$this->registerJs("
+    jQuery(document).ready(function(){
+        $('.home-widget').slider({});
+        $('.animated').fx({
+            fx : {
+                duration: 150,
+                fx : [
+                    'blind',
+                    'bounce',
+                    'clip',
+                    'drop',
+                    'explode',
+                    'pulsate',
+                    'shake',
+                    'slide'
+                ]
+            }
+        });
+    });
+");
